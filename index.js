9f3e725ad6b4b1f43bdcd8ffa792115b69e21000
“DonatelloTHM” <donat.pllana@outlook.com>
2020-08-02 19:29:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+
+function writeCards(arr,name)
+{
+ const arr_new=[];
+    for(let i=0; i<arr.length;i++){
+        arr_new.push(`Thank you, ${arr[i]}, for the wonderful surprise gift!`);
+    }
+    return arr_new;
+}
+
+function countDown(num){
+
+while(num>=0){
+    console.log(num);
+    num--;
+}
+}
